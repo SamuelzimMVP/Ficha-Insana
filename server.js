09601f65fa2d3e25c4d7223ea2a7949b18b2a261
SamuelzimMVP <samuelcaroba08@gmail.com>
2025-12-16 15:03:00 +0000

    Tudo Certo

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-// ######
-// Local onde os pacotes de dependências serão importados
-// ######
+import pkg from "pg";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
 
 // ######
 // Local onde as configurações do servidor serão feitas
 // ######
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
 // ######
 // Local onde as rotas (endpoints) serão definidas
 // ######
-app.get("/", (req, res) => {        
-  // Rota raiz do servidor
-  // Rota GET /
-  // Esta rota é chamada quando o usuário acessa a raiz do servidor
-  // Ela retorna uma mensagem com informações do projeto
+app.get("/", async (req, res) => {
+    // Rota raiz do servidor
+    const db = new Pool({
+        connectionString: process.env.URL_BD,
+    });
 
-  console.log("Rota GET / solicitada"); // Log no terminal para indicar que a rota foi acessada
-  
-  // Responde com um JSON contendo uma mensagem
-  res.json({
-		descricao: "API para não perder a cabeça",    // Substitua pelo conteúdo da sua API
-    autor: "Samuel R. Caroba",     // Substitua pelo seu nome
-  });
+    let dbStatus = "ok";
+    try {
+        await db.query("SELECT 1");
+    } catch (e) {
+        dbStatus = e.message;
+    }
+
+    console.log("Rota GET / solicitada"); // Log no terminal para indicar que a rota foi acessada
+
+    // Responde com um JSON contendo uma mensagem
+    res.json({
+        message: "API para Resgatar Personagem",
+        author: "Carorobinha do 777",
+        statusBD: dbStatus   // Acrescente esta linha
+    });
 });
 
-// ######
-// Local onde o servidor escutar as requisições que chegam
-// ######
+
+
 app.listen(port, () => {
-  console.log(`Serviço rodando na porta:  ${port}`);
+    console.log(`Serviço rodando na porta:  ${port}`);
 });
