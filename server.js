24c4274cd1d7b0810dbad98e94c311ae2ac6b3c9
SamuelzimMVP <samuelcaroba08@gmail.com>
2025-12-16 17:45:42 +0000

    ok

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const { Pool } = pkg; // Obtém o construtor Pool do pacote pg para gerenciar co
 let pool = null; // Variável para armazenar o pool de conexões com o banco de dados
 
 app.use(cors({
-  origin: "https://ficha-fs.vercel.app"
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://ficha-fs.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 app.use(express.json());
 // Função para obter uma conexão com o banco de dados
