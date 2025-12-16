a7aa379a16edbada65eec1e41ecdf8f34b58a1cf
SamuelzimMVP <samuelcaroba08@gmail.com>
2025-12-16 00:58:14 +0000

    att git

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Para Neon
-});
+const createClient = () => {
+  return new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+};
 
-module.exports = pool;
+module.exports = createClient;
